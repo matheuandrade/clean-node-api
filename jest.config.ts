dd48249d470ad0824c9f5a80e97d33356c168404
Matheus Andrade <matheusandrade@MacBook-Pro-de-Matheus.local>
2022-03-07 11:14:25 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  preset: 'ts-jest',
+  collectCoverageFrom: ['<rootDir>/src/**/*ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  roots: ['<rootDir>/src']
+}
