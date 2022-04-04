0aab17e8f707d4002b24a6f9ac4c475bbb2ad8bf
Matheus Andrade <matheus.andrade@ntconsultcorp.com>
2022-04-04 15:12:36 -0300

    test: ensure SignUpController return an error if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -6,7 +6,6 @@ describe('SignUp Controller', () => {
 
     const httpRequest = {
       body: {
-        // name: 'name',
         email: 'email',
         password: 'password',
         passwordConfirmation: 'password'
@@ -17,5 +16,22 @@ describe('SignUp Controller', () => {
 
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+  })
+
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+
+    const httpRequest = {
+      body: {
+        name: 'name',
+        password: 'password',
+        passwordConfirmation: 'password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
   })
 })
